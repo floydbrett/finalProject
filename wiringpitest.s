f13206918099951b93ddc240510872da68a1f536
Chadrick <57962060+MintSquirrel@users.noreply.github.com>
2019-12-08 16:12:00 -0600

    Update wiringpitest.s

--- wiringpitest.s
+++ wiringpitest.s
@@ -1,5 +1,6 @@
 #include <wiringPi.h>
 #include <iostream>
+using namespace std;
 
 int main()
 {
@@ -7,12 +8,17 @@ int main()
 	const int FEEDING_PIN = 17;
 	pinMode(FEEDING_PIN, INPUT);
 	
+	if(wiringPiSetup()<0){
+		cout<<"setup wiring pi failed"<<endl;
+		return 1;
+	}
+	
     while(true)
     {
         // As soon as we detect an input, log and quit.
         if(digitalRead(FEEDING_PIN) == HIGH)
 		{
-			std::cout << ("Button is pressed!\n");	
+			cout << "Button is pressed!\n";	
 			break;
 		}
     }
