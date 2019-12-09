7d9b2c9869b2770525d40663b527af93d800043c
smmr-dn <56598021+smmr-dn@users.noreply.github.com>
2019-12-09 14:06:49 -0600

    Create button.cpp

---
+++ button.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+int main(int argc, char** argv)
+{
+    wiringPiSetup();
+    
+    //18 for button GPIO24, 22 for GPIO25, 12 for GPIO18
+    //This is for GPIO17
+    const int INPUT_PIN = 11;
+    
+    pinMode (INPUT_PIN, INPUT);
+    
+    while(true)
+    {
+        if(digitalRead(INPUT_PIN) == HIGH)
+        {
+            printf("Button is pressed!\n");
+            break;
+        }
+    }
+    
+    return 0;
+}
